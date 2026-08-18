8cf121aa1211da56fcb431ddfd3ff4ae94d4dc3d
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-08-18 08:18:34 +0700

    test: add unit tests for spellcheck

--- tests/bamboo_engine_adapter_test.cpp
+++ tests/bamboo_engine_adapter_test.cpp
@@ -144,7 +144,7 @@ int main() {
   assert(checkedBoundary.deleteCount == 0);
   assert(checkedBoundary.commitText == " ");
 
-  areca::BambooEngineAdapter uncheckedEngine("Telex 2", false);
+  areca::BambooEngineAdapter uncheckedEngine("Telex 2", false, false);
   for (char key : std::string("awbc")) {
     type(uncheckedEngine, key);
   }
@@ -153,8 +153,17 @@ int main() {
   assert(uncheckedBoundary.deleteCount == 0);
   assert(uncheckedBoundary.commitText == " ");
 
-  areca::BambooEngineAdapter modernToneEngine("Telex 2", true, true);
-  areca::BambooEngineAdapter traditionalToneEngine("Telex 2", true, false);
+  areca::BambooEngineAdapter basicSpellcheckEngine("Telex 2", true, false);
+  for (char key : std::string("awbc")) {
+    type(basicSpellcheckEngine, key);
+  }
+  auto basicBoundary = type(basicSpellcheckEngine, ' ');
+  assert(basicBoundary.currentText == "ăbc");
+  assert(basicBoundary.deleteCount == 3);
+  assert(basicBoundary.commitText == "awbc ");
+
+  areca::BambooEngineAdapter modernToneEngine("Telex 2", true, true, true);
+  areca::BambooEngineAdapter traditionalToneEngine("Telex 2", true, true, false);
   areca::BambooResult modernTone;
   areca::BambooResult traditionalTone;
   for (char key : std::string("hoaf")) {
@@ -178,7 +187,7 @@ int main() {
   assert(std::find(charsets.begin(), charsets.end(), "Unicode tổ hợp") !=
          charsets.end());
   for (const auto &charset : charsets) {
-    areca::BambooEngineAdapter encodedEngine("Telex 2", true, true, charset);
+    areca::BambooEngineAdapter encodedEngine("Telex 2", true, true, true, charset);
     type(encodedEngine, 'a');
     const auto encodedResult = type(encodedEngine, 's');
     assert(fcitx::utf8::validate(encodedResult.newText));
@@ -192,7 +201,7 @@ int main() {
   assert(vniResult.newText == "ấ");
 
   areca::BambooEngineAdapter combiningEngine(
-      "Telex 2", true, true, "Unicode tổ hợp");
+      "Telex 2", true, true, true, "Unicode tổ hợp");
   type(combiningEngine, 'a');
   const auto combiningResult = type(combiningEngine, 's');
   assert(combiningResult.newText == "a\u0301");
@@ -201,7 +210,7 @@ int main() {
 
   const std::vector<areca::MacroDefinition> macros = {
       {"bt", "Be There"}, {"vn", "Việt Nam"}};
-  areca::BambooEngineAdapter macroEngine("Telex 2", true, true, "Unicode",
+  areca::BambooEngineAdapter macroEngine("Telex 2", true, true, true, "Unicode",
                                          true, true, macros);
   display.clear();
   for (char key : std::string("bt ")) {
@@ -214,7 +223,7 @@ int main() {
   assert(display == "be there ");
 
   areca::BambooEngineAdapter uppercaseMacroEngine(
-      "Telex 2", true, true, "Unicode", true, true, macros);
+      "Telex 2", true, true, true, "Unicode", true, true, macros);
   display.clear();
   for (char key : std::string("BT ")) {
     applyToDisplay(display, type(uppercaseMacroEngine, key));
@@ -222,7 +231,7 @@ int main() {
   assert(display == "BE THERE ");
 
   areca::BambooEngineAdapter disabledMacroEngine(
-      "Telex 2", true, true, "Unicode", false, true, macros);
+      "Telex 2", true, true, true, "Unicode", false, true, macros);
   display.clear();
   for (char key : std::string("bt ")) {
     applyToDisplay(display, type(disabledMacroEngine, key));
@@ -230,7 +239,7 @@ int main() {
   assert(display == "bt ");
 
   areca::BambooEngineAdapter encodedMacroEngine(
-      "Telex 2", true, true, "Unicode tổ hợp", true, false, macros);
+      "Telex 2", true, true, true, "Unicode tổ hợp", true, false, macros);
   display.clear();
   for (char key : std::string("vn ")) {
     applyToDisplay(display, type(encodedMacroEngine, key));
